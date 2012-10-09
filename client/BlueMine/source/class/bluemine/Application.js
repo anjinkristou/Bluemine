6812d7369e37c5ce8f7871815ee8edf33ca96137
Mehrez Kristou <anjin.kristou@gmail.com>
2012-10-09 13:01:00 +0900

    Header working

--- client/BlueMine/source/class/bluemine/Application.js
+++ client/BlueMine/source/class/bluemine/Application.js
@@ -31,6 +31,10 @@ qx.Class.define("bluemine.Application",
 
   members :
   {
+    // UI Components
+    __header : null,
+    __mode : null,
+    
     /**
      * This method contains the initial application code and gets called 
      * during startup of the application
@@ -59,15 +63,96 @@ qx.Class.define("bluemine.Application",
 
       this.getRoot().add(this._getLayout(), {edge:0});
     },
+    
     _getLayout: function() {
-      var composite = new qx.ui.container.Composite(new qx.ui.layout.Dock(0, 0));   
-      composite.add(new bluemine.view.Header(), {edge:'north'});
+      var composite = new qx.ui.container.Composite(new qx.ui.layout.Dock(0, 0));
+      this.__header = new bluemine.view.Header();　
+      this.__header.addListener("changeMode", this._onChangeMode, this);
+      
+      composite.add(this.__header, {edge:'north'});
       composite.add(this._getMenu(), {edge:'north'});
       composite.add(this._getButtons(), {edge:'west'});
       composite.add(this._getTree(), {edge:'center'});
       composite.add(this._getStatusBar(), {edge:'south'});
       return composite;
     },
+    
+    // ***************************************************
+    // MODE HANDLING
+    // ***************************************************
+    /**
+     * Event handler for changing the mode of the palyground.
+     * @param e {qx.event.type.Data} The data event containing the mode.
+     */
+    _onChangeMode : function(e) {
+      var mode = e.getData();
+      // ignore setting the same mode
+      if (mode == this.__mode) {
+        return;
+      }
+
+      if (!this.setMode(mode)) {
+        this.__header.setMode(e.getOldData());
+      } else {
+        // select the first sample
+        this.setCurrentSample(this.__samples.getFirstSample(mode));
+      }
+    },
+    
+    /**
+     * Setter and dispatcher for the current mode the playground is in.
+     * @param mode {String} The mode to use.
+     */
+    setMode : function(mode) {
+      // check if the mode is supported
+      if (!this.__supportsMode(mode)) {
+        throw new Error("Mode '" + mode + "' not supported");
+      }
+
+      // only set new mode if not already set
+      if (this.__mode == mode) {
+        return true;
+      }
+
+      // only change the mode if no code gets lost
+      if (this.__discardChanges()) {
+        return false;
+      }
+
+      // store the mode
+      qx.bom.Cookie.set("currentMode", mode, 100);
+      this.__mode = mode;
+
+      // update the views
+      confirm(this.tr("Click OK to discard your changes.")
+
+      return true;
+    },
+    
+    // ***************************************************
+    // UPDATE & RUN & COMPARE
+    // ***************************************************
+    /**
+     * Checcks if the code is changed. If that is the case, the user will be
+     * prompted to discard the changes.
+     *
+     * @lint ignoreDeprecated(confirm)
+     * @return {Boolean} <code>true</code> if the code has been modified
+     */
+    __discardChanges : function() {
+    /*
+      var userCode = this.__editor.getCode();
+      if (userCode && this.__isCodeNotEqual(userCode, this.getOriginCode()))
+      {
+        if (!confirm(this.tr("Click OK to discard your changes.")))
+        {
+          return true;
+        }
+      }
+    */
+      return false;
+    },
+
 
     _getStatusBar: function() {
       var composite = new qx.ui.container.Composite(new qx.ui.layout.HBox());
